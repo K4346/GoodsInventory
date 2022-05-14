ae4cdb3cdbc95713a9454d0f177e9daa34a1dd90
K4346 <laeyenix@gmail.com>
2022-05-14 21:23:00 +0300

    upd

--- app/src/main/java/com/executor/goodsinventory/domain/env/ImageUtils.java
+++ app/src/main/java/com/executor/goodsinventory/domain/env/ImageUtils.java
@@ -15,42 +15,11 @@ limitations under the License.
 
 package com.executor.goodsinventory.domain.env;
 
-import android.graphics.Bitmap;
 import android.graphics.Matrix;
-import android.os.Environment;
-
-import java.io.File;
-import java.io.FileOutputStream;
 
 /** Utility class for manipulating images. */
 public class ImageUtils {
-  // This value is 2 ^ 18 - 1, and is used to clamp the RGB values before their ranges
-  // are normalized to eight bits.
-  static final int kMaxChannelValue = 262143;
 
-  private static int YUV2RGB(int y, int u, int v) {
-    // Adjust and check YUV values
-    y = (y - 16) < 0 ? 0 : (y - 16);
-    u -= 128;
-    v -= 128;
-
-    // This is the floating point equivalent. We do the conversion in integer
-    // because some Android devices do not have floating point in hardware.
-    // nR = (int)(1.164 * nY + 2.018 * nU);
-    // nG = (int)(1.164 * nY - 0.813 * nV - 0.391 * nU);
-    // nB = (int)(1.164 * nY + 1.596 * nV);
-    int y1192 = 1192 * y;
-    int r = (y1192 + 1634 * v);
-    int g = (y1192 - 833 * v - 400 * u);
-    int b = (y1192 + 2066 * u);
-
-    // Clipping RGB values to be inside boundaries [ 0 , kMaxChannelValue ]
-    r = r > kMaxChannelValue ? kMaxChannelValue : (r < 0 ? 0 : r);
-    g = g > kMaxChannelValue ? kMaxChannelValue : (g < 0 ? 0 : g);
-    b = b > kMaxChannelValue ? kMaxChannelValue : (b < 0 ? 0 : b);
-
-    return 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
-  }
 
   /**
    * Returns a transformation matrix from one reference frame into another. Handles cropping (if
